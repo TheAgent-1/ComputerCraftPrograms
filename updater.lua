39447a3106e32384b6619ffe3d1863f112085989
TheAgent-1 <108107690+TheAgent-1@users.noreply.github.com>
2025-02-19 17:02:34 +1300

    Update updater.lua

--- updater.lua
+++ updater.lua
@@ -19,6 +19,39 @@ local function downloadFile(url, filename) --Handles downloading the selected fi
     end
 end
 
+local function MailInstall() --Handles installing the mail system
+    term.clear()
+    term.setCursorPos(1, 1)
+    print("Please select the installation type:")
+    print("1 - Server")
+    print("2 - Client")
+    write("Select an option: ")
+
+    local choice = read()
+    if choice == "1" then
+        downloadFile(mailserver_url, "MailServer.lua")
+    elseif choice == "2" then
+        downloadFile(mailclient_url, "MailClient.lua")
+    else
+        print("Invalid option. Exiting.")
+    end
+end
+
+local function MusicInstall() --Handles installing the music player
+    term.clear()
+    term.setCursorPos(1, 1)
+    print("Music Player is currently broken.")
+    print("Would you like to download it anyway? (Y/N)")
+    local choice = read()
+    if choice == "Y" then
+        downloadFile(musicplayer_url, "MusicPlayer.lua")
+    elseif choice == "N" then
+        print("Installation cancelled.")
+    else
+        print("Invalid option. Exiting.")
+    end
+end
+
 local function main() --Handles main screen
     term.clear()
     term.setCursorPos(1, 1)
@@ -29,12 +62,16 @@ local function main() --Handles main screen
     write("Select an option: ")
 
     local choice = read()
-
-    if choice == "1" then
+    if choice == "0" then
+        term.clear()
+        term.setCursorPos(1, 1)
+        return
+    elseif choice == "1" then
         MailInstall()
    
     elseif choice == "2" then
         MusicInstall()
+
     else
         print("Invalid option. Exiting.")
     end
